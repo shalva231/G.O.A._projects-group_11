c0b6fa8c8ba3f612bfe564ac5c033a08c82289f1
shalva231 <shubitidzeshalva30@gmail.com>
2024-05-05 17:14:34 +0400

    GOA

---
+++ day(21-30+1)/day_26/comework/homework.py
@@ -0,0 +1,69 @@
+'''
+1. Manual Sum Function: 
+Create a function called manual_sum that iterates over list and adds their sum in a variable, 
+then returns variable. Use for loop for this task.
+'''
+# def manual_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         sum += i
+#     return sum
+
+'''
+2. Manual Max Function: Define a function named manual_max that iterates through list, 
+updating a variable with the maximum value, 
+then returns the maximum value found. 
+Use for loop for this task.
+'''
+# def manual_max(arr):
+#     max = arr[0]
+#     for i in arr:
+#         if i > max:
+#             max = i
+#     return max
+
+'''
+3. Manual Min Function: Define a function named manual_min that iterates through list, 
+updating a variable with the minimum value, 
+then returns the minimum value found. 
+Use for loop for this task.
+'''
+# def manual_min(arr):
+#     min = arr[0]
+#     for i in arr:
+#         if i < min:
+#             min = i
+#     return min
+
+
+'''
+4. Manual Len Function: 
+Develop a function named manual_len that iterates through list, 
+counting each element, 
+and returns the count as the length of the list. 
+Use for loop for this task.
+'''
+
+# def manual_len(arr):
+#     count = 0
+#     for i in arr:
+#         count += 1
+#     return count
+
+
+'''
+5. Copy function of reduce: 
+define a function named manual_reduce that takes a list as input, 
+then create an empty list named copied_list to store the copied items inside it. 
+use for loop to loop over each item in the original list, 
+append them to the copied_iterable list.
+In the end, return the copied_iterable after iterating through all items.
+finally demonstrate the usage of the manual_reduce function by creating an original list, 
+making a copy of it, and printing both the original and copied lists.
+'''
+
+# def manual_reduce(arr):
+#     copied_list = []
+#     for i in arr:
+#         copied_list.append(i)
+#     return copied_list
