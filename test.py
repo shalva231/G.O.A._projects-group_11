390f07ffd55e5b70bead7baf4fc5ae1b6bf2f529
shalva231 <shubitidzeshalva30@gmail.com>
2024-05-10 18:23:36 +0400

    GOA

--- test.py
+++
@@ -1,6 +0,0 @@
-def reverse_words(text):
-    words = text.split(" ")
-    reversed_words = []
-    for word in words:
-        reversed_words.append(word[::-1])
-    return " ".join(reversed_words)
