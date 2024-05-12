660da5b5f669465eaa36cfbea6cb87d9f524b15d
shalva231 <shubitidzeshalva30@gmail.com>
2024-05-12 20:29:31 +0400

    GOA

---
+++ test.py
@@ -0,0 +1,9 @@
+#მომხმარებელს უნდა შემოატანინოთ რიცხვი და 
+#შემდეგ უნდა დაბეჭდოთ არის თუ არა ეს რიცხვი ხუთის ჯერადი
+
+num = int(input("please enter a number: "))
+
+if num % 5 == 0:
+    print(" the number you entered is a multiple of 5")
+else:
+    print(" the number you entered is not a multiple of 5")
