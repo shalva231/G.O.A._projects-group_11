24b60d7b2114d826da76a04e3f8c00ebf9f6dca5
shalva231 <shubitidzeshalva30@gmail.com>
2024-05-15 22:41:14 +0400

    GOA

--- independent/projects/rock paper scissors/rock_paper_Scissors.py
+++ independent/projects/rock paper scissors/rock_paper_Scissors.py
@@ -37,74 +37,87 @@ drawing = [
 
 choice_end = '''_______________________________________________'''
 
-#get a random choice for player two and find the index of the choice
+count = 0
+
+p1 = ""
+p2 = ""
+
+disconected = random.choice([
+"Network issues" , 
+"Weak signal or interference" , 
+"driver issues" , 
+"software-related problems"
+])
+
+if count == 0:
+    print("\n \n searching fo a lobby.... ")
+    count += 1
+else:
+    print("\n \n searching fo a new lobby.... ")
+time.sleep(2)
+print("!lobby found!")
+print("\n")
+time.sleep(2)
+
 p2 = random.choice(choice)
 p2_index = choice.index(p2)
-
 p2_final = f" ----------\n player 2's choice \n ---------- \n {p2} \n {drawing[p2_index]}"
 
 
-count = 0
-p1 = ""
-while p1 != "quit":
-    if count == 0:
-        print("\n \n searching fo a lobby.... ")
-        count += 1
-    else:
-        print("\n \n searching fo a new lobby.... ")
-    time.sleep(2)
-    print("!lobby found!")
-    print("\n")
-    time.sleep(2)
+p1 = input("please choose(rock , paper , scissors):  ")
+while p1 not in choice:
+    print("!!invalid!!! \n .try again.")
+    p1 = input("please choose(rock , paper , scissors):  ")
 
-    p1 = input("please choose(rock , paper , scissors) (type 'quit' to quit):  ")
-    while p1 not in choice:
-        print("!!invalid!!! \n .try again.")
-        p1 = input("please choose(rock , paper , scissors):  ")
-
-    else:
-        p1_index = choice.index(p1)
-        p1_final = f" ----------\n player 1's choice \n ---------- \n {p1} \n {drawing[p1_index]}"
+else:
+    p1_index = choice.index(p1)
+    p1_final = f" ----------\n player 1's choice \n ---------- \n {p1} \n {drawing[p1_index]}"
 
 
-    if p1 == p2:
-        result = f"p1 and p2 had a draw in this game"
+if p1 == p2:
+    result = f"p1 and p2 had a draw in this game"
 
 
-    #[ "rock" , "paper" , "scissors"]
-    #[    0        1           2    ]
+#[ "rock" , "paper" , "scissors"]
+#[    0        1           2    ]
+elif p1 == choice[0] and p2 == choice[2] or p1 == choice[1] and p2 == choice[0] or p1 == choice[2] and p2 == choice[1]:
+    result = f"p1 won this game by playing a {p1} \n ||!!CONGRATS!!||"
+else:
+    result = f"p2 won this game by playing a {p2}"
 
-    elif p1 == choice[0] and p2 == choice[2] or p1 == choice[1] and p2 == choice[0] or p1 == choice[2] and p2 == choice[1]:
-        result = f"p1 won this game by playing a {p1} \n ||!!CONGRATS!!||"
-    else:
-        result = f"p2 won this game by playing a {p2}"
+time.sleep(1)
+print("loading p1 results... ")
+time.sleep(5)
+print("[loaded] \n")
 
-    time.sleep(1)
-    print("loading p1 results... ")
-    time.sleep(5)
-    print("[loaded] \n")
+time.sleep(1)
+print(p1_final)
+print(choice_end)
 
-    time.sleep(1)
-    print(p1_final)
-    print(choice_end)
+time.sleep(1)
+print("loading p2 results... ")
+time.sleep(5)
+print("[loaded] \n")
 
-    time.sleep(1)
-    print("loading p2 results... ")
-    time.sleep(5)
-    print("[loaded] \n")
+time.sleep(1)
+print(p2_final)
+print(choice_end)
 
-    time.sleep(1)
-    print(p2_final)
-    print(choice_end)
+time.sleep(1)
+print("loading results... ")
+time.sleep(5)
+print("[loaded] \n")
 
-    time.sleep(1)
-    print("loading results... ")
-    time.sleep(5)
-    print("[loaded] \n")
+time.sleep(1)
+print("\n",result)
+print(choice_end)
 
-    time.sleep(1)
-    print("\n",result)
-    print(choice_end)
+
+
+print("\n")
+print(choice_end)
+print(f"disconected why? ({disconected})")
+print(choice_end)
 
     
 
