61bb58b49832e54bcdeb068dfd227a5351f10e29
shalva231 <shubitidzeshalva30@gmail.com>
2024-05-26 17:05:27 +0400

    goa

--- independent/projects/hangman/hangman.py
+++ independent/projects/hangman/hangman.py
@@ -132,30 +132,38 @@ def hang_man():
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("You already guessed that letter.")
+                
             elif guess not in word:
                 print(f"{guess} is not in the word.")
                 tries += 1
                 guessed_letters.append(guess)
+
             else:
                 print(f"Good job! {guess} is in the word.")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
                 indices = [i for i, letter in enumerate(word) if letter == guess]
+                
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
+
                 if "_" not in word_completion:
                     guessed = True
+
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guessed that word.")
+
             elif guess != word:
                 print(f"{guess} is not the word.")
                 tries += 1
                 guessed_words.append(guess)
+
             else:
                 guessed = True
                 word_completion = word
+
         else:
             print("Not a valid guess.")
         
