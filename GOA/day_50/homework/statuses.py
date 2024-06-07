c34fb748f25c03f747d2d41251b48f3cc384c682
shalva231 <shubitidzeshalva30@gmail.com>
2024-06-07 18:41:57 +0400

    GOA

---
+++ GOA/day_50/homework/statuses.py
@@ -0,0 +1,22 @@
+status = [
+    "This file does not contain any data.",
+    "The file is devoid of content.",
+    "No content is present in this file.",
+    "The file is empty.",
+    "There is no data in this file.",
+    "This file is blank.",
+    "The file contains no information.",
+    "There is nothing in this file.",
+    "The file is devoid of any data.",
+    "This file has no content.",
+    "The file is hollow.",
+    "There are no entries in this file.",
+    "The file is vacuous.",
+    "This file is devoid of any information.",
+    "The file is barren.",
+    "No data is found in this file.",
+    "This file is void of data.",
+    "The file is clear of content.",
+    "This file contains no text.",
+    "The file is stark." 
+]
