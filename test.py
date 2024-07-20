574d11cd043698ef81ffb8adf0ee3d518a489957
shalva231 <shubitidzeshalva30@gmail.com>
2024-07-21 00:04:24 +0400

    here

---
+++ test.py
@@ -0,0 +1,12 @@
+# and operator
+print(False and False) # False
+print(False and True) # False
+print(True and False) # False
+print(True and True) # True
+
+# or operator
+print(False or False) # False
+print(False or True) # True
+print(True or False) # True
+print(True or True) # True
+
